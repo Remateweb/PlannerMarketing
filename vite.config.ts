0c33e5a97a5f026ab5547556a1210de312650a07
Mauricio Corrêa <mauriciocorrea@192.168.0.20>
2025-11-06 20:20:46 -0300

    fix vite.js

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
+// ⚠️ o base DEVE bater exatamente com o nome do repositório
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/PlannerMarketing/",
+});
