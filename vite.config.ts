aec359e390b77966d531ff82d01908aef8175dc6
Mauricio Corrêa <mauriciocorrea@192.168.0.20>
2025-11-06 20:26:49 -0300

    Corrige base para GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// ✅ usa "/" porque o projeto está na raiz da branch publicada
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  base: "/PlannerMarketing/",
 });
