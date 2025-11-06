776b6301f1f7817cdb032960e8f02e297c1dd1fa
Mauricio Corrêa <mauriciocorrea@192.168.0.20>
2025-11-06 20:33:21 -0300

    Deploy corrigido para GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// ⚠️ use o nome EXATO do repositório entre barras
 export default defineConfig({
   plugins: [react()],
   base: "/PlannerMarketing/",
+  build: {
+    outDir: "dist",
+  },
 });
